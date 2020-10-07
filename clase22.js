554b6cc0766c12399c3edcdb1f5c8d463bb0cb04
Sergio Corzo <shndzcorzo@gmail.com>
2020-10-06 21:41:34 -0500

    Se modificó clase22.js

--- clase22.js
+++ clase22.js
@@ -1,7 +1,8 @@
-function Persona( nombre, apellido, altura ){
+function Persona( nombre, apellido, altura, edad ){
     this.nombre = nombre
     this.apellido = apellido
     this.altura = altura
+    this.mayorDeEdad = () => this.edad >= 18
 }
 
 Persona.prototype.saludar = function() {
@@ -19,9 +20,11 @@ Persona.prototype.soyAlto = function( ){
     // Comparacion       valor devuel si es true  Valor devuelto si es false
 }
 
-var sergio = new Persona('Serio', 'Corzo', 1.68 )
+var sergio = new Persona('Serio', 'Corzo', 1.68, 28 )
 sergio.saludar()
 sergio.soyAlto()
-var edduin = new Persona('Edduin', 'climaco', 1.73)
+var edduin = new Persona('Edduin', 'climaco', 1.73, 17)
 edduin.saludar()
 edduin.soyAlto()
+console.log(sergio.mayorDeEdad())
+console.log(edduin.mayorDeEdad())
