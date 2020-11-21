a4730dcae160fcfe708463cc7177dc3a63ac9a9b
Sergio Corzo <shndzcorzo@gmail.com>
2020-11-21 14:47:23 -0600

    Se modifico los archivos clase30.js y clase30.html. Se agregaron los archivos clase31.js y clase21.html. Practicas de manejo de errores en Jquery con .fail.

--- clase30.js
+++ clase30.js
@@ -1,3 +1,5 @@
+// Manejar errores en momento de ejecucion con
+// $.fail de JQuery
 // Para hacer un request o petición debemos saber la dirección URL
 // de la API que usaremos
 const API_URL = 'https://swapi.dev/api/'
@@ -7,28 +9,30 @@ const PEOPLE_URL = 'people/:id/'
 // cambiar o remplazar caracteres dentro de un estring ejecución
 const opts = { crossDomain: true }
 
-function obtenerPersonaje( id ){
-    
-    return new Promise(function(resolve, reject){
-        const url = `${API_URL}${PEOPLE_URL.replace(`:id`, id)}`
+function obtenerPersonaje( id, callback ){
+    const url = `${API_URL}${PEOPLE_URL.replace(`:id`, id)}`
+    $
+    .get( url, opts, callback )
+    .fail( () =>
+     console.log(`Sucedio un error. \nNo se pudo obtener el id: ${id}`)
+    )
 
-        $.get(url, opts, function( personaje ){
-            resolve( personaje )
-        })
-        .fail(()=> reject(id))
-       
-    } )
-
-    
 }
+
 //A esta forma de hacer codigo y lidiar con el asincronismo
 //se le conoce como callback hell
-
-obtenerPersonaje( 1 )
-.then( function( personaje ){
-    console.log(`El personaje 1 es ${personaje.name}`)  
-     })
-    .catch( id =>{
-        console.log(`Sucedio un problema al obtener el id: ${id}`)  
-    } )
-    
+obtenerPersonaje( 1, function( personaje ){
+    console.log(`Hola, yo soy ${personaje.name}`)
+    obtenerPersonaje( 2, function( personaje ){
+        console.log(`Hola, yo soy ${personaje.name}`)
+        obtenerPersonaje( 3, function( personaje ){
+            console.log(`Hola, yo soy ${personaje.name}`)
+            obtenerPersonaje( 4, function( personaje ){
+                console.log(`Hola, yo soy ${personaje.name}`)
+                obtenerPersonaje( 5, function( personaje ){
+                    console.log(`Hola, yo soy ${personaje.name}`)
+                })
+            })
+        })
+    })
+} )
